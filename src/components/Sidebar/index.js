2ff35f31266988baa0c1a6cf4f38fbdb1d44b2aa
yoonisali <yoonismahamoudali@gmail.com>
2024-02-12 01:23:31 -0800

    Add social media links to Sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import './index.scss';
 import LogoY from '../../assets/images/logo-y.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser, } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className='nav-bar'>
@@ -22,6 +23,23 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/yoonisali/'>
+          <FontAwesomeIcon icon={faLinkedin}  color='#4d4d4e' />
+        </a>
+      </li>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://github.com/yoonisali'>
+          <FontAwesomeIcon icon={faGithub}  color='#4d4d4e' />
+        </a>
+      </li>
+      <li>
+        <a target='_blank' rel='noreferrer' href='https://www.instagram.com/yoonis24/'>
+          <FontAwesomeIcon icon={faInstagram}  color='#4d4d4e' />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
