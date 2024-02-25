5b6b072be772df0d34eb8202d7da88d4e8cf6165
yoonisali <yoonismahamoudali@gmail.com>
2024-02-25 02:30:48 -0800

    Add dynamic letter animation to Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,43 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-  const nameArray = ['Y', 'o', 'o', 'n', 'n', 'i', 's']
-  const jobArray = ['W', 'e', 'b',]
+  const nameArray = [' ', 'Y', 'o', 'o', 'n', 'i', 's']
+  const jobArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+  // useEffect(() => {
+  //   return setTimeout(() => {
+  //     setLetterClass('text-animate-hover')
+  //   }, 4000)
+  // }, [])
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+    
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br/> I'm Yoonis
-        <br />
-        Web Developer
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <AnimatedLetters letterClass={letterClass}
+            strArray={nameArray}
+            idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass}
+            strArray={jobArray}
+            idx={15} />
         </h1>
         <h2>Fullstack Developer / Frontend Expert</h2>
         <Link to="/contact" className="flat-button">CONTACT ME</Link>
